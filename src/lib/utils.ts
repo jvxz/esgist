e3f55af3f43e640d16ba2284f7808a61bc346a75
jamie 🌨️ <jamie@wisp.bio>
2025-04-26 16:59:48 -0400

    feat: create withcancel helper wrapper

---
+++ src/lib/utils.ts
@@ -0,0 +1,15 @@
+import * as p from '@clack/prompts'
+
+/**
+ * wraps a function in a cancelable promise
+ */
+export async function withCancel(fn: () => Promise<string | symbol | undefined> | undefined) {
+  const res = await fn()
+
+  if (p.isCancel(res)) {
+    p.outro('Aborting...')
+    process.exit(1)
+  }
+
+  return res
+}
