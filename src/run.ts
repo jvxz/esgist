59f5b0c7d4ab2d50457fa865be2395af08460b58
jamie 🌨️ <jamie@wisp.bio>
2025-04-27 17:09:17 -0400

    feat: add prepare procedure

--- src/run.ts
+++ src/run.ts
@@ -1,14 +1,18 @@
-import { Effect } from 'effect'
+import { Console, Effect } from 'effect'
 import { fetchGistData } from './sequences/fetch-gist-data'
 import { gistLinkPrompt } from './sequences/gist-link'
 import { handleConfigDeps } from './sequences/handle-config-deps'
+import { prepare } from './sequences/prepare'
 
 export const run = Effect.gen(function* () {
+  const {
+    configFilename,
+  } = yield* prepare
+
   const gistLink = yield* gistLinkPrompt
   const gistData = yield* fetchGistData(gistLink)
-  const configDeps = yield* handleConfigDeps(gistData)
-
-  // yield* Console.log(gistContent)
+  yield* handleConfigDeps(gistData.deps)
+  yield* Console.log(gistData.content)
 
   return gistData
 })
---
+++ src/sequences/prepare.ts
@@ -0,0 +1,147 @@
+import { readdir, readFile } from 'node:fs/promises'
+import * as p from '@clack/prompts'
+import c from 'ansis'
+
+import { Data, Effect, Option } from 'effect'
+import { execa } from 'execa'
+import { abort, withCancel } from '../lib/utils'
+
+class PrepError extends Data.TaggedError('PrepError')<{
+  cause?: unknown
+  message?: string
+}> {}
+
+const handleUncommittedChanges = Effect.gen(function* (_) {
+  const gitStatus = yield* _(
+    Effect.tryPromise({
+      try: async () => execa('git', ['status', '--porcelain']),
+      catch: e => new PrepError({
+        cause: e,
+        message: 'Failed to get git status',
+      }),
+    }),
+    Effect.option,
+  )
+
+  if (Option.isNone(gitStatus)) return Effect.void
+
+  const confirm = yield* Effect.tryPromise({
+    try: async () => withCancel(async () => p.confirm({
+      message: 'There are uncommitted changes. Continue anyway?',
+      initialValue: false,
+    })),
+    catch: e => new PrepError({
+      cause: e,
+      message: 'Failed to confirm uncommitted changes',
+    }),
+  })
+
+  if (!confirm) return abort()
+
+  return Effect.void
+})
+
+const handleNodeProject = Effect.gen(function* (_) {
+  const packageJson = yield* _(Effect.tryPromise(async () => readFile('package.json')), Effect.option)
+
+  if (Option.isNone(packageJson)) {
+    const confirm = yield* Effect.tryPromise({
+      try: async () => withCancel(async () => p.confirm({
+        message: 'You don\'t seem to be in a Node.js project. Continue anyway?',
+        initialValue: true,
+      })),
+      catch: e => new PrepError({
+        cause: e,
+        message: 'Failed to read package.json',
+      }),
+    })
+
+    if (confirm) return true
+  }
+
+  return false
+})
+
+const getConfig: Effect.Effect<PrepareResult['configFilename'] | void, PrepError> = Effect.gen(function* (_) {
+  const dir = yield* _(Effect.tryPromise(async () => readdir(process.cwd())), Effect.option)
+
+  if (Option.isNone(dir)) {
+    p.log.warn('Could not scan directory. Defaulting to gist file name')
+    return yield* Effect.void
+  }
+
+  const config = yield* _(
+    Effect.succeed(dir.value.filter(e => e.includes('eslint'))),
+    Effect.map(e => e.filter(e => e.includes('js'))),
+  )
+
+  // const _ignore = yield* _(
+  //   Effect.fromNullable(dir.value.find(e => e.includes('.eSLintignore'))),
+  //   Effect.option,
+  // )
+
+  if (config.length === 0) return yield* Effect.void
+
+  if (config.length > 1) {
+    const confirm = yield* Effect.tryPromise({
+      try: async () => withCancel(async () => p.confirm({
+        message: 'Found multiple ESLint config files. Select to overwrite?',
+        initialValue: true,
+      })),
+      catch: e => new PrepError({
+        cause: e,
+        message: 'Failed to confirm ESLint config file',
+      }),
+    })
+
+    if (!confirm) return abort()
+
+    const selectedFile = yield* Effect.tryPromise({
+      try: async () => withCancel(async () => p.select({
+        message: 'Select the desired file',
+        options: config.map((e) => {
+          return {
+            label: e,
+            value: e,
+          }
+        }),
+      })),
+      catch: e => new PrepError({
+        cause: e,
+        message: 'Failed to select ESLint config file',
+      }),
+    })
+
+    return selectedFile
+  }
+
+  const confirm = yield* Effect.tryPromise({
+    try: async () => withCancel(async () => p.confirm({
+      message: `ESLint config file found: ${c.yellow(config[0])}. Overwrite?`,
+      initialValue: true,
+    })),
+    catch: e => new PrepError({
+      cause: e,
+      message: 'Failed to confirm ESLint config file',
+    }),
+  })
+
+  if (!confirm) return abort()
+
+  return config[0]
+})
+
+interface PrepareResult {
+  configFilename: string
+}
+
+export const prepare = Effect.gen(function* () {
+  yield* handleNodeProject
+  yield* handleUncommittedChanges
+  const configFilename = yield* getConfig
+
+  return {
+    configFilename,
+  } as PrepareResult
+})
+
